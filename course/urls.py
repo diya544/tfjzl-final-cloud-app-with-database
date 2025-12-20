cf129c14f7743e3f64413e1dd889d6f68d46157a
Diya Ghosh <diyaslg2004@gmail.com>
2025-12-20 12:24:54 +0530

    Changes

--- course/urls.py
+++ course/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list, name='course_list'),  # new homepage
+    # List all courses
+    path('', views.course_list, name='course_list'),
+
+    # Show lesson + quiz
     path('lesson/<int:lesson_id>/', views.lesson_detail, name='lesson_detail'),
+
+    # REQUIRED FOR TASK 6 ✅
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+
+    # REQUIRED FOR TASK 6 ✅
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
 ]
